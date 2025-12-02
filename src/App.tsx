e7ecd9d451098e246651b93a08cbe1808d927a1e
ekhrapykin <EugeneKhrapykin@melco-resorts.com>
2025-12-02 16:03:09 +0800

    add TodoList component and integrate with App for displaying tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, useCallback } from 'react';
 import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 import type { Todo } from './types/Todo';
 import { v4 as uuidv4 } from 'uuid';
 import _ from 'lodash';
@@ -16,11 +17,20 @@ function App() {
     setTodos(_.concat([todo], todos));
   }, [todos]);
 
+  const deleteTodo = useCallback((id: string) => {
+    setTodos(_.filter(todos, (todo) => todo.id !== id));
+  }, [todos]);
+
+  const editTodo = useCallback((updatedTodo: Todo) => {
+    setTodos(_.map(todos, (todo) => (todo.id === updatedTodo.id ? updatedTodo : todo)));
+  }, [todos]);
+
   return (
     <Container maxWidth="md" sx={{ py: 4 }}>
       <Typography variant="h4" component="h1" gutterBottom>
         OstrideLabs Tasks list
       </Typography>
+      <TodoList todos={todos} onDelete={deleteTodo} onEdit={editTodo} />
       <TodoForm onSubmit={addTodo} />
     </Container>
   );
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,24 @@
+import type { Todo } from '../types/Todo';
+import { Box, Typography } from '@mui/material';
+import _ from 'lodash';
+
+interface TodoListProps {
+  todos: Todo[];
+  onDelete: (id: string) => void;
+  onEdit: (todo: Todo) => void;
+}
+
+function TodoList({ todos }: TodoListProps) {
+  if (_.isEmpty(todos)) {
+    return <Typography variant="body1" sx={{ textAlign: 'center', padding: 3, color: 'text.secondary' }}>ToDo list is Empty!</Typography>;
+  }
+
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+      {_.map(todos, (todo) => (todo.id)
+        )}
+    </Box>
+  );
+}
+
+export default TodoList;
